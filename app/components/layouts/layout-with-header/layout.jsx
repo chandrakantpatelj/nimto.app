095b844418f7ee43eefd4c8753292a306e255832
dhruvi-jspit <dhruvi@jspinfotech.com>
2025-08-07 17:50:00 +0530

    added create template design

---
+++ app/components/layouts/layout-with-header/layout.jsx
@@ -0,0 +1,40 @@
+import { useEffect } from 'react';
+import { TemplateHeader } from '@/app/(with-header)/templates/design/components';
+
+function LayoutWithHeader({ children }) {
+  useEffect(() => {
+    const bodyClass = document.body.classList;
+
+    // Add a class to the body element
+    bodyClass.add('demo1');
+    bodyClass.add('sidebar-fixed');
+    bodyClass.add('header-fixed');
+
+    const timer = setTimeout(() => {
+      bodyClass.add('layout-initialized');
+    }, 1000); // 1000 milliseconds
+
+    // Remove the class when the component is unmounted
+    return () => {
+      bodyClass.remove('demo1');
+      bodyClass.remove('sidebar-fixed');
+      bodyClass.remove('sidebar-collapse');
+      bodyClass.remove('header-fixed');
+      bodyClass.remove('layout-initialized');
+      clearTimeout(timer);
+    };
+  }, []);
+  return (
+    <>
+      <div className="custom-wrapper ps-0 flex grow flex-col">
+        <TemplateHeader />
+
+        <main className="grow" role="content">
+          {children}
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default LayoutWithHeader;
