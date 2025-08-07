5f48cf64834441e85be21318b8ad78e5efd3f6b5
dhruvi-jspit <dhruvi@jspinfotech.com>
2025-08-07 11:20:20 +0530

    added remaining routes

---
+++ app/(protected)/reportings/content.jsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { Card } from '@/components/ui/card';
+import UserStatics from './components/UserStatics';
+
+export function Reporting() {
+  return (
+    <Card className="p-4">
+      <UserStatics />
+    </Card>
+  );
+}
