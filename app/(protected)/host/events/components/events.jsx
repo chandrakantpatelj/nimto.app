e48b3c166a930d2ad942fd069b76ec99f271a44f
dhruvi-jspit <dhruvi@jspinfotech.com>
2025-08-19 11:24:51 +0530

    added create ai pop up and other changes

--- app/(protected)/host/events/components/events.jsx
+++ app/(protected)/host/events/components/events.jsx
@@ -93,7 +93,7 @@ const Events = () => {
         <div className="relative">
           <Search className="size-4 text-muted-foreground absolute start-3 top-1/2 -translate-y-1/2" />
           <Input
-            placeholder="Search by name, email, or role..."
+            placeholder="Search your events..."
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
             className="ps-9 w-95"
