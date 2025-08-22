6fcbf23ed9c277dc2abcabd7524b7deae79c5180
dhruvi-jspit <dhruvi@jspinfotech.com>
2025-08-22 13:04:14 +0530

    show host design based on login users role and other file structure changes

--- app/(blank-layout)/events/[eventId]/content.jsx
+++ app/(blank-layout)/events/[eventId]/content.jsx
@@ -1,10 +1,10 @@
 'use client';
 
 import { useState } from 'react';
-import { TemplateHeader } from '../../components';
-import Step1 from '../../components/Step1';
-import Step2 from '../../components/Step2';
-import Step3 from '../../components/Step3';
+import { TemplateHeader } from '../components';
+import Step1 from '../components/Step1';
+import Step2 from '../components/Step2';
+import Step3 from '../components/Step3';
 
 function EditEventContent() {
   const [activeStep, setActiveStep] = useState(0);
--- app/(protected)/events/select-template/page.jsx
+++ app/(protected)/events/select-template/page.jsx
@@ -1,6 +1,6 @@
 import { Fragment } from 'react';
 import Link from 'next/link';
-import { ArrowLeft } from 'lucide-react';
+import { CirclePlus } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Container } from '@/components/common/container';
 import {
@@ -9,8 +9,13 @@ import {
   ToolbarHeading,
 } from '@/components/common/toolbar';
 import { PageNavbar } from '@/app/(protected)/account/page-navbar';
-import { ToolbarPageTitle } from '@/app/components/partials/common/toolbar';
-import { ManageGuestContent } from './content';
+import {
+  ToolbarDescription,
+  ToolbarPageTitle,
+} from '@/app/components/partials/common/toolbar';
+import { SelectEventContent } from './content';
+
+// import { EventManagement } from './content';
 
 function EventManagementPage() {
   return (
@@ -19,19 +24,19 @@ function EventManagementPage() {
       <Container>
         <Toolbar>
           <ToolbarHeading>
-            <ToolbarPageTitle text="Manage Invitations: Birthday Party" />
+            <ToolbarPageTitle text="Create Event: Select a Template" />
+            <ToolbarDescription>Super Admin (Super Admin)</ToolbarDescription>
           </ToolbarHeading>
           <ToolbarActions>
-            <Button variant="secondary" appearance="ghost" asChild>
-              <Link href="/host/events">
-                <ArrowLeft /> Back
-              </Link>
+            <Button variant="outline" asChild>
+              <Link href="/events">Cancel</Link>
             </Button>
           </ToolbarActions>
         </Toolbar>
       </Container>
       <Container>
-        <ManageGuestContent />
+        <SelectEventContent />
+        {/* <EventManagement /> */}
       </Container>
     </Fragment>
   );
